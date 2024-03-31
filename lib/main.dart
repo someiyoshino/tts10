3f6cc908ebf356c5dd22aed307cd504a0014e4c7
yoshida <yoshi-2020-qqh@jinmusoftware.com>
2024-04-01 02:49:55 +0900

    再生確認OK

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,13 +16,59 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyWidget(),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Material3 Theme'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MyWidget()),
+                );
+              },
+              child: const Text('Go to MyWidget'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
 class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+  MyWidget({super.key}) {
+    initializeTts();
+  }
+  final FlutterTts tts = FlutterTts();
+  final String _text1 = 'Hello, World!. ABC.';
+  final String _text2 = 'おはよう。こんにちは。松田です。';
+
+  initializeTts() async {
+    await tts.setLanguage('ja-JA');
+    await tts.setSpeechRate(0.6);
+    await tts.setPitch(1.0);
+    await tts.awaitSpeakCompletion(false); // 発話の完了まで待機。
+    await tts.awaitSynthCompletion(true); // ファイルの合成まで待機。
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,9 +77,20 @@ class MyWidget extends StatelessWidget {
         title: const Text('Material3 Theme'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {},
-          child: const Text('Elevated Button'),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                await tts.speak(_text1);
+              },
+              child: const Text('Hello'),
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  await tts.speak(_text2);
+                },
+                child: const Text('おはよう')),
+          ],
         ),
       ),
     );
